9b21123f1e1eb3f59af67d198fe48f61ae57bcc0
Jindarath <32261408+Jindarath@users.noreply.github.com>
2019-09-27 18:02:49 +0700

    Game Logic : เปลี่ยน welcome เป็น helloworld

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import WordCard from './WordCard';
-const word = "Hello";
 const word = "WELCOME";
+const word = "HELLOWORLD";
 
 
 class App extends Component {
